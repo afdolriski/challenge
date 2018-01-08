39ea6ecbe431d863b56df370b3724deda2629218
Afdol Rizki <afdolriski@gmail.com>
2018-01-08 16:54:39 +0700

    main file

--- app.py
+++ app.py
@@ -3,31 +3,38 @@ from flask import Flask, g, jsonify
 from api import myapi
 from core import db, oauth
 from mixer.backend.flask import mixer
+from resources.albums import albums_api
+from resources.comments import comments_api
+from resources.photos import photos_api
+from resources.posts import posts_api
+from resources.todos import todos_api
 from resources.users import users_api
-from resources.todos import todos_api
-from validator import RequestValidator
 
+from validator import RequestValidator
 import config
-from resources.todos import todos_api
+
+url_prefix = '/api/v1'
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object('config')
 
-    db.init_app(app)
-    
+    db.init_app(app)    
     oauth.init_app(app)
     oauth._validator = RequestValidator()
 
-    app.register_blueprint(myapi)
-    app.register_blueprint(todos_api)
-    app.register_blueprint(users_api)
+    app.register_blueprint(myapi, url_prefix=url_prefix)
+    app.register_blueprint(todos_api, url_prefix=url_prefix)
+    app.register_blueprint(users_api, url_prefix=url_prefix)
+    app.register_blueprint(posts_api, url_prefix=url_prefix)
+    app.register_blueprint(photos_api, url_prefix=url_prefix)
+    app.register_blueprint(comments_api, url_prefix=url_prefix)
+    app.register_blueprint(albums_api, url_prefix=url_prefix)
 
     return app
 
 if __name__ == '__main__':
-    # Enable Flask-OAuthlib logging for this application.
     import logging
     logger = logging.getLogger('flask_oauthlib')
     logger.addHandler(logging.StreamHandler())
